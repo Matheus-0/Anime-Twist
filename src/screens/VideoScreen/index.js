68a55f848067289d4db248f37455213633d0d399
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2020-12-10 11:10:30 -0300

    Not show error after pressing next episode button.

--- src/screens/VideoScreen/index.js
+++ src/screens/VideoScreen/index.js
@@ -214,6 +214,7 @@ const VideoScreen = ({
     nextEpisodeViewActive.current = false;
     videoCompletePosition.current = null;
 
+    setShowError(false);
     setVideoIsLoading(true);
     setEpisodePlaying(nextEpisode);
 
