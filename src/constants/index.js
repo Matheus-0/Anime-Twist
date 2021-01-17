e097bff6a536f8347e9f8396e2d1fa9added53c7
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2021-01-16 22:05:05 -0300

    Update access token, base URL and decryption key.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const accessToken = '1rj2vRtegS8Y60B3w3qNZm5T2Q0TN2NR';
-export const baseURL = 'https://twist.moe';
+export const accessToken = '0df14814b9e590a1f26d3071a4ed7974';
+export const baseURL = 'https://api.twist.moe';
 export const CDN = 'https://twistcdn.bunny.sh';
-export const key = 'LXgIVP&PorO68Rq7dTx8N^lP!Fa5sGJ^*XK';
+export const key = '267041df55ca2b36f2e322d05ee2c9cf';
 export const userAgent = require('../../app.json').expo.version;
