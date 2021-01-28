614b498ee3780e7e81cad81a4f25a30f4ff98c9f
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2021-01-27 21:25:05 -0300

    Update CDN.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const accessToken = '0df14814b9e590a1f26d3071a4ed7974';
 export const baseURL = 'https://api.twist.moe';
-export const CDN = 'https://twistcdn.bunny.sh';
+export const CDN = 'https://at-cdn.bunny.sh';
 export const key = '267041df55ca2b36f2e322d05ee2c9cf';
 export const userAgent = require('../../app.json').expo.version;
