143f61b469961e7d1e0bfe1982bfee80da5e49f3
Matheus Alves <57298342+Matheus-0@users.noreply.github.com>
2021-03-13 13:25:00 -0300

    Change to default CDN.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const baseURL = 'https://api.twist.moe';
-export const CDN = 'https://air-cdn.twist.moe';
+export const CDN = 'https://cdn.twist.moe';
 export const key = '267041df55ca2b36f2e322d05ee2c9cf';
 export const referer = 'https://twist.moe';
 export const token = '0df14814b9e590a1f26d3071a4ed7974';
